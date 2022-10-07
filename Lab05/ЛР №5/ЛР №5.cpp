24b20072995d49c73771a562d3f317769f225f1e
alafyte <nroussak0405@gmail.com>
2022-10-07 21:04:53 +0300

    Лабораторная работа №5

---
+++ Lab05/ЛР №5/ЛР №5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void main()
+{
+    setlocale(0, "Rus");
+
+    string str;
+    stack <char> stack1;
+    int countOpen = 0, countEnd = 0;
+
+    cout << "Введите строку: " << endl;
+    cin >> str;
+
+    for (auto c : str)
+    {
+        if (c == '(' || c == '[' || c == '{')
+            countOpen++;
+        if (c == ')' || c == '}' || c == ']')
+            countEnd++;
+    }
+    if (countOpen != countEnd)
+        cout << "Скобки расставлены неверно" << endl;
+    else {
+        for (auto c : str)
+        {
+            if (c == '(' || c == '[' || c == '{')
+                stack1.push(c);
+
+            if (c == ')' || c == '}' || c == ']')
+            {
+                if (c == ')' && stack1.top() == '(')
+                    stack1.pop();
+                else if (c == ']' && stack1.top() == '[')
+                    stack1.pop();
+                else if (c == '}' && stack1.top() == '{')
+                    stack1.pop();
+                else {
+                    cout << "Скобки расставлены неверно" << endl;
+                    exit(0);
+                }
+            }
+        }
+         cout << "Скобки расставлены верно" << endl;
+    }
+}
